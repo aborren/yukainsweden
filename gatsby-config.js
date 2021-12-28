5e8d88f9b100cc446cb02400d16716730586f96b
Dan Isacson <dan_isacson@hotmail.com>
2021-12-28 23:58:05 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Yuka in Sweden",
+    description: "Yuka in Sweden blog",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
